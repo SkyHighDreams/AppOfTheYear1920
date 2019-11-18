5fb48c135f8aba63d3533ef0bfa3b82a1ce5b5e9
Thibaut <weekx.t@gmail.com>
2019-11-18 14:37:10 +0100

    Injesh kaz posjnostem

---
+++ app/src/main/java/com/student/lukasz/appoftheyear19_20/Second_Activity.java
@@ -0,0 +1,14 @@
+package com.student.lukasz.appoftheyear19_20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Second_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_);
+    }
+    //injeshkash poz nostem
+}
