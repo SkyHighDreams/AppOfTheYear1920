dfd8a07b3e8c2be6166b11937d34a58e1e7e4fbf
Thibaut <weekx.t@gmail.com>
2019-11-18 15:30:36 +0100

    Button naar second activity gaan

--- app/src/main/java/com/student/lukasz/appoftheyear19_20/MainActivity.java
+++ app/src/main/java/com/student/lukasz/appoftheyear19_20/MainActivity.java
@@ -1,7 +1,10 @@
 package com.student.lukasz.appoftheyear19_20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-    //elle moe op ne stok
+public void NextScreen(View view) {
+    Intent myIntent = new Intent(MainActivity.this, Second_Activity.class);
+    MainActivity.this.startActivity(myIntent);
+}
 }
