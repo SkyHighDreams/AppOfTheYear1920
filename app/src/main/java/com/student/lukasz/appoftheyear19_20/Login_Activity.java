6339c1c9ca7e2175b2ec1ee31e341cfced4c89d8
Lukasz Nalbach <lukasz.nalbach1@gmail.com>
2019-11-22 16:01:02 +0100

    login + pass lbl toegevoegd aan login act back button + functionaliteit

--- app/src/main/java/com/student/lukasz/appoftheyear19_20/Login_Activity.java
+++ app/src/main/java/com/student/lukasz/appoftheyear19_20/Login_Activity.java
@@ -1,13 +1,22 @@
 package com.student.lukasz.appoftheyear19_20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login_Activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+    }
+
+    public void Backtologinscreen(View view)
+    {
+        Intent myIntent = new Intent(Login_Activity.this, MainActivity.class);
+        Login_Activity.this.startActivity(myIntent);
     }
 }
