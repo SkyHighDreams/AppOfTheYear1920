80ef41f3143e234acb13708a8e990c98710f63ec
Lukasz Nalbach <lukasz.nalbach1@gmail.com>
2019-12-09 14:54:26 +0100

    added btn backgrounds

--- app/src/main/java/com/student/lukasz/appoftheyear19_20/Activity_4_answer.java
+++ app/src/main/java/com/student/lukasz/appoftheyear19_20/Activity_4_answer.java
@@ -1,7 +1,9 @@
 package com.student.lukasz.appoftheyear19_20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_4_answer extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Activity_4_answer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_answer);
+    }
+
+    public void Tologinscreen(View view)
+    {
+        Intent myIntent = new Intent(Activity_4_answer.this , Login_Activity.class);
+        Activity_4_answer.this.startActivity(myIntent);
     }
 }
