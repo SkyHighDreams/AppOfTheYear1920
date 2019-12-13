6e541429577e9f18ce5588deaa31500f80bb76bf
Thibaut <weekx.t@gmail.com>
2019-12-13 16:34:50 +0100

    Nieuwe StartActivity gemaakt

---
+++ app/src/main/java/com/student/lukasz/appoftheyear19_20/StartScreen.java
@@ -0,0 +1,17 @@
+package com.student.lukasz.appoftheyear19_20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+
+}
